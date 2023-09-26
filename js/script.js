6720b1d60619da6876cb2e41a68bc59c6331d5a8
Humza Emmad <humzaemmad@gmail.com>
2023-09-26 10:29:22 +0500

    Add One round of play

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ function getComputerChoice() {
   const pick= ["rock", "paper", "scissor"];
   return pick[Math.floor(Math.random() * pick.length)];
 }
+//One Round of Rock Paper Scissor
+function playRound(playerChoice, computerChoice) {
+  if(playerChoice === computerChoice){
+    return `It's a tie! you both picked ${playerChoice}`;
+  } else if (playerChoice === "rock" && computerChoice === "scissor"){
+    return "You win!, Rock beats Scissor";
+  } else if (playerChoice === "paper" && computerChoice === "rock"){
+    return "You win!, Paper beats Rock";
+  } else if (playerChoice === "scissor" && computerChoice === "paper"){
+    return "You win!, Scissor beats Paper";
+  } else if (playerChoice === "rock" && computerChoice === "paper"){
+    return "You Lost!, Paper beats Rock";
+  } else if (playerChoice === "paper" && computerChoice === "scissor"){
+    return "You Lost!, Scissor beats Paper";
+  } else if (playerChoice === "scissor" && computerChoice === "rock"){
+    return "You Lost!, Rock beats scissor";
+  }
+}

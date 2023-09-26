0b51416804f9431dce0be354dd7b3a339d053a15
Humza Emmad <humzaemmad@gmail.com>
2023-09-26 10:27:59 +0500

    Add Computer Random Choice

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-let UserChoice=parseInt(prompt("Please choose 0 for rock,1 for paper or 2 for scissor"));
-console.log(UserChoice);
-if (UserChoice>2) {
-console.log("You selected the wrong number. YOU LOSE!");
-} 
-let CompChoice = getRandomInt(3);
-console.log("Computer Chose",CompChoice);
-if (UserChoice>CompChoice) {
-    console.log("YOU WIN!");
-} else if(UserChoice===CompChoice {
-    console.log("ITS A DRAW);
+//Comouter random choice
+function getComputerChoice() {
+  const pick= ["rock", "paper", "scissor"];
+  return pick[Math.floor(Math.random() * pick.length)];
 }
